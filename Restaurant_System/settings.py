16514339c1f3b426c22510c73f3dfafcda99d45b
EzekielMichael <ezekielm905@gmail.com>
2024-06-04 23:07:35 +0300

    Update settings for PostgreSQL and add .env file to .gitignore

--- Restaurant_System/settings.py
+++ Restaurant_System/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c8p-18wm)d3*cp+zp%pf&v$xu^_q*6fs0gym%!*czu@b-y%dyr'
-
+#SECRET_KEY = 'django-insecure-c8p-18wm)d3*cp+zp%pf&v$xu^_q*6fs0gym%!*czu@b-y%dyr'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-c8p-18wm)d3*cp+zp%pf&v$xu^_q*6fs0gym%!*czu@b-y%dyr')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -71,9 +76,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Restaurant_System.wsgi.application'
 
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -84,15 +88,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restaurant_Sell',
+#         'USER': 'postgres',
+#         'PASSWORD': '@#Nkhotya1',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restaurant_Sell',
-        'USER': 'postgres',
-        'PASSWORD': '@#Nkhotya1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
